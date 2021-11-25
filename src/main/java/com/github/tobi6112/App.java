a20b30dabf5adba6ddcf8be0d946f607abf1c77f
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-11-25 16:22:02 +0100

    feat: Initial commit

---
+++ src/main/java/com/github/tobi6112/App.java
@@ -0,0 +1,13 @@
+package com.github.tobi6112;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
